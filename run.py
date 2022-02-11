76caf58429592b992bd7bf94bc8f8ffed7d09fa1
Brian Foo <brian@youaremyjoy.org>
2022-02-11 13:46:10 -0500

    implement large arc and resolution logic

--- run.py
+++ run.py
@@ -22,6 +22,7 @@ parser.add_argument('-fps', dest="FRAMES_PER_SECOND", default=30, type=int, help
 parser.add_argument('-img', dest="MAP_IMAGE_FILE", default="img/map.png", help="Background map image file")
 parser.add_argument('-color', dest="LINE_COLOR", default="#ff2e2e", help="Color of line")
 parser.add_argument('-line', dest="LINE_WIDTH", default=4, type=int, help="Width of line")
+parser.add_argument('-res', dest="RESOLUTION", default=1, type=int, help="Multiply resolution by this much")
 parser.add_argument('-frames', dest="OUTPUT_FRAMES", default="output/frames/", help="Output frames directory")
 parser.add_argument('-out', dest="OUTPUT_VIDEO", default="output/aviation_visualization.mp4", help="Output video if applicable (must also include -map flag)")
 parser.add_argument('-debug', dest="DEBUG", action="store_true", help="Just output debug image?")
@@ -52,18 +53,20 @@ routes = routes[:1000]
 # pprint(airports[0])
 # pprint(routes[0])
 
+w = a.WIDTH * a.RESOLUTION
+h = a.HEIGHT * a.RESOLUTION
+
 for i, airport in enumerate(airports):
     lat, lon = (airport['Latitude'], airport['Longitude'])
     nx, ny = (norm(lon, (-180, 180)), norm(lat, (90, -90)))
-    x, y = (nx * a.WIDTH, ny * a.HEIGHT)
+    x, y = (nx * w, ny * h)
 
     airports[i]['x'] = x
     airports[i]['y'] = y
 
 airportLookup = createLookup(airports, "Airport ID")
 
-w = a.WIDTH
-h = a.HEIGHT
+
 # baseIm = Image.new(mode="RGBA", size=(w, h), color=(0,0,0,0))
 baseIm = Image.open(a.MAP_IMAGE_FILE)
 baseIm = baseIm.convert("RGBA")
@@ -74,6 +77,58 @@ baseIm = baseIm.resize((w, h), resample=Image.LANCZOS)
 # baseIm.save("output/debug.png")
 # sys.exit()
 
+def drawArc(fromX, fromY, toX, toY, im, progress):
+    global a
+    global w
+    global h
+
+    midX, midY = midpoint((fromX, fromY), (toX, toY))
+    radiusA = distance(fromX, fromY, midX, midY)
+    radiusB = radiusA / 2
+    lineWidth = a.LINE_WIDTH * a.RESOLUTION
+    padding = lineWidth * 2
+
+    angleBetweenPoints = 0
+    if fromY == toY:
+        angleBetweenPoints = 0
+    elif fromX < toX:
+        angleBetweenPoints = angleBetween(fromX, fromY, toX, toY)
+    elif fromX > toX:
+        angleBetweenPoints = angleBetween(toX, toY, fromX, fromY)
+    elif fromX == toX:
+        angleBetweenPoints = 90
+
+    fromDegrees = -180
+    toDegrees = 0
+    isReverseDirection = (fromX > toX)
+
+    if fromY > h * 0.5 and toY > h * 0.5:
+        fromDegrees = 0
+        toDegrees = 180
+        isReverseDirection = (fromX < toX)
+
+    x0 = padding
+    y0 = padding + (radiusA - radiusB)
+    x1 = x0 + radiusA * 2
+    y1 = y0 + radiusB * 2
+    arcImW = arcImH = roundInt(radiusA * 2 + padding * 2)
+    arcIm = Image.new(mode="RGBA", size=(arcImW, arcImH), color=(0,0,0,0))
+    arcDraw = ImageDraw.Draw(arcIm)
+    arcDraw.arc([x0, y0, x1, y1], fromDegrees, toDegrees, a.LINE_COLOR, lineWidth)
+    arcIm = arcIm.rotate(angle=-angleBetweenPoints)
+    arcX = roundInt(midX - arcImW * 0.5)
+    arcY = roundInt(midY - arcImH * 0.5)
+    offsetX = 0
+    offsetY = 0
+    if arcX < 0:
+        offsetX = -arcX
+        arcX = 0
+    if arcY < 0:
+        offsetY = -arcY
+        arcY = 0
+    im.alpha_composite(arcIm, (arcX, arcY), (offsetX, offsetY))
+
+progress = 1
 routeCount = len(routes)
 for i, route in enumerate(routes):
     sourceId = str(route["Source airport ID"])
@@ -96,37 +151,22 @@ for i, route in enumerate(routes):
         print(f'Source and destination is the same for route {route["Source airport ID"]} to {route["Destination airport ID"]}')
         continue
 
-    midX, midY = midpoint((fromX, fromY), (toX, toY))
-    radiusA = distance(fromX, fromY, midX, midY)
-    radiusB = radiusA / 2
-    padding = a.LINE_WIDTH * 2
 
-    angleBetweenPoints = 0
-    if fromY == toY:
-        angleBetweenPoints = 0
-    elif fromX < toX:
-        angleBetweenPoints = angleBetween(fromX, fromY, toX, toY)
-    elif fromX > toX:
-        angleBetweenPoints = angleBetween(toX, toY, fromX, fromY)
-    elif fromX == toX:
-        angleBetweenPoints = 90
+    deltaX = abs(fromX - toX)
+    if deltaX <= w*0.5:
+        drawArc(fromX, fromY, toX, toY, baseIm, progress)
 
-    fromDegrees = -180
-    toDegrees = 0
+    # distance is too far; go in the other direction (thus need to draw two arcs)
+    else:
+        fromX1, fromY1, toX1, toY1 = (fromX, fromY, toX-w, toY)
+        fromX2, fromY2, toX2, toY2 = (fromX+w, fromY, toX, toY)
 
-    x0 = padding
-    y0 = padding + (radiusA - radiusB)
-    x1 = x0 + radiusA * 2
-    y1 = y0 + radiusB * 2
-    arcImW = arcImH = roundInt(radiusA * 2 + padding * 2)
-    arcIm = Image.new(mode="RGBA", size=(arcImW, arcImH), color=(0,0,0,0))
-    arcDraw = ImageDraw.Draw(arcIm)
-    arcDraw.arc([x0, y0, x1, y1], fromDegrees, toDegrees, a.LINE_COLOR, a.LINE_WIDTH)
-    arcIm = arcIm.rotate(angle=-angleBetweenPoints)
-    arcX = roundInt(midX - arcImW * 0.5)
-    arcY = roundInt(midY - arcImH * 0.5)
-    if arcX > 0 and arcY > 0:
-        baseIm.alpha_composite(arcIm, (arcX, arcY))
+        if fromX > toX:
+            fromX1, fromY1, toX1, toY1 = (fromX, fromY, toX+w, toY)
+            fromX2, fromY2, toX2, toY2 = (fromX-w, fromY, toX, toY)
+
+        drawArc(fromX1, fromY1, toX1, toY1, baseIm, progress)
+        drawArc(fromX2, fromY2, toX2, toY2, baseIm, progress)
 
     printProgress(i+1, routeCount)
 
